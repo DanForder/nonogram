1b2aa13358903369c8a6b9c519e55b8fe056a0b2
Dan Forder <danfrdr@gmail.com>
2021-12-29 12:30:31 +0000

    added boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import "./styles/main.scss";
+import HomeView from "./views/Home/Home";
 
-function App() {
+const Test = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <h1>Hello again</h1>
+      <Link to="/">Home</Link>
+    </Layout>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route index element={<HomeView />} />
+      <Route path="/nested" element={<Test />} />
+    </Routes>
+  );
+};
 
 export default App;
