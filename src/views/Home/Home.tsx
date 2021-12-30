ffd777d6edbfb753c4e280533e6f08abce554f7f
Dan Forder <danfrdr@gmail.com>
2021-12-30 17:35:19 +0000

    added contents to home screen

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
 import Layout from "../../components/Layout/Layout";
+import { puzzles } from "../../data/puzzles";
 
 export const HomeView = () => {
+  const puzzleContents = puzzles.map((_puzzle, index) => (
+    <>
+      <Link key={`puzzle-${index + 1}`} to={`puzzles/${index + 1}`}>
+        Puzzle {index + 1}
+      </Link>
+      <br />
+    </>
+  ));
+
   return (
     <Layout>
       <h1>Welcome to Nonogram Puzzler</h1>
-      <Link to="puzzles/1">Puzzle 1</Link>
+      {puzzleContents}
     </Layout>
   );
 };
